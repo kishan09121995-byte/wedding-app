e2b07b85c2e37679a7ed6a06b5cd9d179c70725e
Kishan Batavia <batavia.kishan9@gmail.com>
2026-04-30 15:16:46 +0530

    Cleanup: Remove duplicate .js files

--- src/store/authStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-export const useAuthStore = create((set) => ({
-    user: null,
-    role: null,
-    loading: true,
-    setUser: (user) => set({ user }),
-    setRole: (role) => set({ role }),
-    setLoading: (loading) => set({ loading }),
-}));
